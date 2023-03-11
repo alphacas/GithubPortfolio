b99c3e1c120529026317ccbbd496ef15c592420d
Edmund Fahrenheit <Fischerprogram@gmail.com>
2023-03-11 18:38:04 -0500

    csv text reviewed

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -12,18 +12,20 @@ const Resume: NextPage<GithubData> = memo(({education, experience, skills}) => {
   return (
     <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
       <div className="flex flex-col divide-y-2 divide-neutral-300">
-        {education.length ? 
-        <ResumeSection title="Education">
-          {education.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection> : null } 
-        {experience.length ? 
-        <ResumeSection title="Work">
-          {experience.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection> : null}
+        {education.length ? (
+          <ResumeSection title="Education">
+            {education.map((item, index) => (
+              <TimelineItem item={item} key={`${item.title}-${index}`} />
+            ))}
+          </ResumeSection>
+        ) : null}
+        {experience.length ? (
+          <ResumeSection title="Work">
+            {experience.map((item, index) => (
+              <TimelineItem item={item} key={`${item.title}-${index}`} />
+            ))}
+          </ResumeSection>
+        ) : null}
         <ResumeSection title="Skills">
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             {skills.map((skillgroup, index) => (
